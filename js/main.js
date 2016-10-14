885bdb086095c41bb22b9adc1fb4e937400b7074
Brenton Cozby <brenton.cozby@gmail.com>
2016-10-14 17:14:35 -0400

    adjust animation

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@
         showContentButtons[i].addEventListener('click', function() {
             if (this.classList.contains('clicked-button')) {
                 this.classList.remove('clicked-button');
+                this.style.marginBottom = '110px';
                 this.parentNode.childNodes[3].style.padding = '100px 20px 20px 20px';
                 $(this.parentNode.childNodes[7]).slideUp(300);
             } else {
                 this.classList.add('clicked-button');
+                this.style.marginBottom = '10px';
                 this.parentNode.childNodes[3].style.padding = '20px 20px 20px 20px';
                 $(this.parentNode.childNodes[7]).slideDown(300);
             }
