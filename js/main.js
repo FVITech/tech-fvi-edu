d8931f3dfcf434e4a9c231bbea38d7fb7a355fda
Brenton Cozby <bcozby@fvi.edu>
2016-10-17 14:31:52 -0400

    add fixed-button class and code; change images

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
 ! function() {
-    // on button click, change button style and show content
+
     var showContentButtons = document.getElementsByClassName('show-content-button');
+
+    // on button click, change button style and show content
     for (var i = 0, x = showContentButtons.length; i < x; i++) {
         showContentButtons[i].addEventListener('click', function() {
             if (this.classList.contains('clicked-button')) {
                 this.classList.remove('clicked-button');
-                this.style.marginBottom = '110px';
-                this.parentNode.childNodes[3].style.padding = '100px 20px 20px 20px';
+                this.parentNode.style.padding = '100px 0 140px';
                 $(this.parentNode.nextSibling.nextSibling).slideUp(600);
             } else {
                 this.classList.add('clicked-button');
-                this.style.marginBottom = '10px';
-                this.parentNode.childNodes[3].style.padding = '50px 20px 20px 20px';
+                this.parentNode.style.padding = '20px 0';
                 $(this.parentNode.nextSibling.nextSibling).slideDown(600);
             }
         });
     }
 
+    // on window scroll, fixed clicked button to screen
+    window.addEventListener('scroll', function() {
+        for (var i = 0, x = showContentButtons.length; i < x; i++) {
+            if (showContentButtons[i].classList.contains('clicked-button')) {
+                var contentPosition = showContentButtons[i].parentNode.nextSibling.nextSibling.getBoundingClientRect();
+                if (contentPosition.top <= '0' && contentPosition.bottom >= '50') {
+                    showContentButtons[i].classList.add('fixed-button');
+                } else {
+                    showContentButtons[i].classList.remove('fixed-button');
+                }
+            }
+        }
+    });
 
 }();
