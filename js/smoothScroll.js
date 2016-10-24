b94dd6ae6cb3335a1f274eda42de96db5fab9be1
Brenton Cozby <brenton.cozby@gmail.com>
2016-10-23 21:49:58 -0400

    comment out and delete unecessary code

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,49 +1,21 @@
 $(document).ready(function() {
 
     $(function() {
-        // This will select everything with the class smoothScroll
-        // This should prevent problems with carousel, scrollspy, etc...
         $('.smoothScroll').click(function() {
             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 if (target.length) {
                     // topPadding is the height of the nav, so it scrolls past the nav
-                    var topPadding = ($(target[0]).hasClass('banner')) ? ((window.innerWidth >= '980') ? -51 : 0) : 0;
-                    var timing = (window.scrollY == $(target[0]).offset().top + topPadding) ? 0 : 700;
+                    // var topPadding = ($(target[0]).hasClass('banner')) ? ((window.innerWidth >= '980') ? -51 : 0) : 0;
+                    var timing = (window.scrollY == $(target[0]).offset().top /* + topPadding */) ? 0 : 700;
                     $('body, html').animate({
-                        scrollTop: target.offset().top + topPadding
-                    }, timing, 'easeInOutQuad', function() {
-                        if($(target[0]).has('span.plus-button')) {
-                            var nodes = target[0].childNodes;
-                            for(var i = 0, x = nodes.length; i < x; i++) {
-                                if(nodes[i].classList == 'plus-button') {
-                                    $(nodes[i]).click();
-                                }
-                            }
-                        }
-                    });
+                        scrollTop: target.offset().top // + topPadding
+                    }, timing, 'easeInOutQuad');
                     return false;
                 }
             }
         });
     });
-
-    //Check to see if the window is top; if not then display back-to-top button
-    // $(window).scroll(function() {
-    //     if ($(this).scrollTop() > 100) {
-    //         $('.back-to-top a').fadeIn();
-    //     } else {
-    //         $('.back-to-top a').fadeOut();
-    //     }
-    // });
-
-    //Click event to scroll to top
-    // $('.back-to-top').click(function() {
-    //     $('html, body').animate({
-    //         scrollTop: 0
-    //     }, 400, 'ease-in-out');
-    //     return false;
-    // });
 
 });
