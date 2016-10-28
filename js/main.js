1944e7ade5b0f6a4f3fd0e3c6bbec49d3cf182be
Brenton Cozby <bcozby@fvi.edu>
2016-10-28 13:43:51 -0400

    cache some jquery elements

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
 
+    var plusButtons = document.getElementsByClassName('plus-button');
+    var webBanners = document.getElementsByClassName('web-banner');
+    var cyberBanners = document.getElementsByClassName('cyber-banner');
+    var webNavItems = document.getElementsByClassName('web-nav-item');
+    var cyberNavItems = document.getElementsByClassName('cyber-nav-item');
+    var webLanding = document.getElementsByClassName('web-landing');
+    var cyberLanding = document.getElementsByClassName('cyber-landing');
+    var mobileMenuWidth = '920';
+    var topPadding = (window.innerWidth >= mobileMenuWidth) ? -51 : 0;
+
     // Code for home page
     $('.web-dev').click(function() {
         $('.home-main').fadeOut(function() {
             $('.web-main').fadeIn(function() {
-                setupPage();
+                setupPage('web');
             });
         });
     });
@@ -12,32 +22,19 @@ $(document).ready(function() {
     $('.network-admin').click(function() {
         $('.home-main').fadeOut(function() {
             $('.cyber-main').fadeIn(function() {
-                setupPage();
+                setupPage('cyber');
             });
         });
     });
 
-    function setupPage() {
-        // Code for web-dev and network-admin pages below
-        var plusButtons = document.getElementsByClassName('plus-button');
-        var webBanners = document.getElementsByClassName('web-banner');
-        var cyberBanners = document.getElementsByClassName('cyber-banner');
-        var banners = ($('.web-main').css('display') == "block") ? webBanners : ($('.cyber-main').css('display') == "block") ? cyberBanners : undefined;
-        var webNavItems = document.getElementsByClassName('web-nav-item');
-        var cyberNavItems = document.getElementsByClassName('cyber-nav-item');
-        var navItems = ($('.web-main').css('display') == "block") ? webNavItems : ($('.cyber-main').css('display') == "block") ? cyberNavItems : undefined;
-        var webLanding = document.getElementsByClassName('web-landing');
-        var cyberLanding = document.getElementsByClassName('cyber-landing');
-        var landing = ($('.web-main').css('display') == "block") ? webLanding : ($('.cyber-main').css('display') == "block") ? cyberLanding : undefined;
+    function setupPage(page) {
+        var banners = (page == 'web') ? webBanners : cyberBanners;
+        var navItems = (page == 'web') ? webNavItems : cyberNavItems;
+        var landing = (page == 'web') ? webLanding : cyberLanding;
 
         // When click on HOME button
-        $('.web-home-button').click(function() {
-            $('.web-main').fadeOut(function() {
-                $('.home-main').fadeIn();
-            });
-        });
-        $('.cyber-home-button').click(function() {
-            $('.cyber-main').fadeOut(function() {
+        $('.' + page + '-home-button').click(function() {
+            $('.' + page + '-main').fadeOut(function() {
                 $('.home-main').fadeIn();
             });
         });
@@ -46,16 +43,12 @@ $(document).ready(function() {
         for (var i = 0, x = plusButtons.length; i < x; i++) {
             $(plusButtons[i]).on('click', function() {
                 var button = this;
-                var webBanner = (this.parentNode.classList.contains('web-banner')) ? this.parentNode : undefined;
-                var cyberBanner = (this.parentNode.classList.contains('cyber-banner')) ? this.parentNode : undefined;
-                var $banner = (webBanner) ? $(webBanner) : $(cyberBanner);
+                var $banner = (this.parentNode.classList.contains(page + '-banner')) ? $(this.parentNode) : undefined;
                 // if button is clicked and content is displayed
                 if (button.classList.contains('clicked-button')) {
-                    // topPadding is the nav height, so it scrolls past the nav
-                    var topPadding = (window.innerWidth >= '980') ? -51 : 0;
                     var timing = (window.scrollY == $banner.offset().top + topPadding) ? 0 : 700;
                     // first, scroll to top of banner, then change buton style and slideUp the content
-                    $('body, html').animate({
+                    $('body, html').stop().animate({
                         scrollTop: $banner.offset().top + topPadding
                     }, timing, 'easeInOutQuad', function() {
                         button.classList.remove('clicked-button');
@@ -81,18 +74,15 @@ $(document).ready(function() {
         }
 
         window.addEventListener('scroll', function() {
-
             // on window scroll, fixed clicked button to screen
             for (var i = 0, x = plusButtons.length; i < x; i++) {
                 if (plusButtons[i].classList.contains('clicked-button')) {
                     var contentPosition = plusButtons[i].parentNode.nextSibling.nextSibling.getBoundingClientRect();
                     // bottomPadding is the bottom of the content, plus nav height and button translateY
-                    var bottomPadding = (window.innerWidth >= '980') ? '96' : '45';
-                    // topPadding accounts for nav height
-                    var topPadding = (window.innerWidth >= '980') ? 51 : 0;
+                    var bottomPadding = (window.innerWidth >= mobileMenuWidth) ? '96' : '45';
                     if (contentPosition.top <= String(topPadding) && contentPosition.bottom >= bottomPadding) {
                         $(plusButtons[i]).css({
-                            'top': (-(contentPosition.top) + topPadding) + 'px',
+                            'top': (-(contentPosition.top) - topPadding) + 'px',
                             'transition': '0s'
                         });
                     } else {
@@ -105,8 +95,7 @@ $(document).ready(function() {
             }
 
             // on window scroll, add style to nav item if section is in view
-            if (window.innerWidth >= '920') {
-                var topPadding = (window.innerWidth >= '920') ? '51' : '0';
+            if (window.innerWidth >= mobileMenuWidth) {
                 for (var j = 0, y = banners.length; j < y; j++) {
                     if (landing[0].getBoundingClientRect().bottom < '86') {
                         navItems[0].classList.remove('section-in-view');
@@ -141,12 +130,12 @@ $(document).ready(function() {
         });
 
         // Small screens adjustments below
-        if (window.innerWidth < '920') {
+        if (window.innerWidth < mobileMenuWidth) {
             navItems[0].classList.remove('section-in-view');
         }
 
         // mobile-menu show/hide
-        if (window.innerWidth < '920') {
+        if (window.innerWidth < mobileMenuWidth) {
             $('#web-menu-button, #web-overlay, #web-menu-items li a, #cyber-menu-button, #cyber-overlay, #cyber-menu-items li a').click(function() {
                 if ($('#web-menu-button, #cyber-menu-button').html() == '<i class="fa fa-bars" aria-hidden="true"></i> MENU') {
                     $('#web-menu-button, #cyber-menu-button').html('<i class="fa fa-bars" aria-hidden="true"></i> CLOSE');
