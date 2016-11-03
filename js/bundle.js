b12c6795e004572443ad4f9ed74a5d1627a6aa3a
Brenton Cozby <bcozby@fvi.edu>
2016-11-03 16:36:20 -0400

    add images

--- js/bundle.js
+++ js/bundle.js
@@ -49,7 +49,6 @@ $(document).ready(function () {
 
     $('.page-landing.home').fadeIn(300);
 
-    var smoothScroll = require('./smoothScroll');
     var g = require('./globals');
     var plusButtonFunctions = require('./plus-buttons');
     var menu = require('./menu');
@@ -138,7 +137,7 @@ $(document).ready(function () {
     }
 });
 
-},{"./form":1,"./globals":2,"./menu":4,"./plus-buttons":5,"./smoothScroll":6}],4:[function(require,module,exports){
+},{"./form":1,"./globals":2,"./menu":4,"./plus-buttons":5}],4:[function(require,module,exports){
 'use strict';
 
 !function () {
@@ -248,62 +247,4 @@ $(document).ready(function () {
     module.exports.fixed = fixed;
 }();
 
-},{"./globals":2}],6:[function(require,module,exports){
-'use strict';
-
-$(document).ready(function () {
-    "use strict";
-
-    function smoothScroll() {
-        $(function () {
-            // This will select everything with the class smoothScroll
-            // This should prevent problems with carousel, scrollspy, etc...
-            $('.smoothScroll').click(function (e) {
-                e.preventDefault();
-                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                    var target = $(this.hash);
-                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                    if (target.length) {
-                        // topPadding is the height of the nav, so it scrolls past the nav
-                        var topPadding = $(target[0]).hasClass('banner') ? window.innerWidth >= '920' ? -51 : 0 : 0;
-                        var timing = window.scrollY == $(target[0]).offset().top + topPadding ? 0 : 700;
-                        $('body, html').animate({
-                            scrollTop: target.offset().top + topPadding
-                        }, timing, 'easeInOutQuint', function () {
-                            if ($(target[0]).has('span.plus-button')) {
-                                var nodes = target[0].childNodes;
-                                for (var i = 0, x = nodes.length; i < x; i++) {
-                                    if (nodes[i].classList == 'plus-button') {
-                                        $(nodes[i]).click();
-                                    }
-                                }
-                            }
-                        });
-                        return false;
-                    }
-                }
-            });
-        });
-    }
-
-    //Check to see if the window is top; if not then display back-to-top button
-    // $(window).scroll(function() {
-    //     if ($(this).scrollTop() > 100) {
-    //         $('.back-to-top a').fadeIn();
-    //     } else {
-    //         $('.back-to-top a').fadeOut();
-    //     }
-    // });
-
-    //Click event to scroll to top
-    // $('.back-to-top').click(function() {
-    //     $('html, body').animate({
-    //         scrollTop: 0
-    //     }, 400, 'ease-in-out');
-    //     return false;
-    // });
-
-    module.exports.smoothScroll = smoothScroll;
-});
-
-},{}]},{},[3]);
+},{"./globals":2}]},{},[3]);
--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,31 +1,29 @@
 $(document).ready(function() {
     "use strict";
-    $(function() {
-        // This will select everything with the class smoothScroll
-        // This should prevent problems with carousel, scrollspy, etc...
-        $('.smoothScroll').click(function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    // topPadding is the height of the nav, so it scrolls past the nav
-                    var topPadding = ($(target[0]).hasClass('banner')) ? ((window.innerWidth >= '920') ? -52 : 0) : 0;
-                    var timing = (window.scrollY == $(target[0]).offset().top + topPadding) ? 0 : 700;
-                    $('body, html').animate({
-                        scrollTop: target.offset().top + topPadding
-                    }, timing, 'easeInOutQuint', function() {
-                        if ($(target[0]).has('span.plus-button')) {
-                            var nodes = target[0].childNodes;
-                            for (var i = 0, x = nodes.length; i < x; i++) {
-                                if (nodes[i].classList == 'plus-button') {
-                                    $(nodes[i]).click();
-                                }
+    // This will select everything with the class smoothScroll
+    // This should prevent problems with carousel, scrollspy, etc...
+    $('.smoothScroll').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                // topPadding is the height of the nav, so it scrolls past the nav
+                var topPadding = ($(target[0]).hasClass('banner')) ? ((window.innerWidth >= '920') ? -52 : 0) : 0;
+                var timing = (window.scrollY == $(target[0]).offset().top + topPadding) ? 0 : 700;
+                $('body, html').animate({
+                    scrollTop: target.offset().top + topPadding
+                }, timing, 'easeInOutQuint', function() {
+                    if ($(target[0]).has('span.plus-button')) {
+                        var nodes = target[0].childNodes;
+                        for (var i = 0, x = nodes.length; i < x; i++) {
+                            if (nodes[i].classList == 'plus-button') {
+                                $(nodes[i]).click();
                             }
                         }
-                    });
-                    return false;
-                }
+                    }
+                });
+                return false;
             }
-        });
-    })();
+        }
+    });
 });
