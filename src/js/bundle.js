60d641709ee40c93e67d6ca0e12b0c9c806ea912
Brenton Cozby <bcozby@fvi.edu>
2016-11-08 17:06:31 -0500

    fix banner resize on home-button click

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -213,7 +213,7 @@
                 g.$overlay.hide();
                 g.$applyPopUp.hide();
                 g.$plusButtons.filter('.opened').css({ 'top': '0px', 'transition': '.6s' }).removeClass('opened');
-                g.$banners.filter('.shrink').next().hide().removeClass('shrink');
+                g.$banners.filter('.shrink').removeClass('shrink').next().hide();
                 g.$navItems.removeClass('section-in-view');
                 $(window).off('scroll', navItemsStyle);
                 return false;
